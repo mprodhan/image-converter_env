bb10b5be1c0d7a7756486f75282bc09c3ef30196
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2021-07-06 16:32:58 -0400

    created functions for the helper and main function.

--- images.py
+++ images.py
@@ -1,9 +1,9 @@
 import sys
 import os
-from PIL import Image
+from PIL import Image, ImageFilter
 # from PIL ImageFilter
 
-image_folder= sys.argv[1]
+image_folder = sys.argv[1]
 output_images = sys.argv[2]
 
 # print(image_folder, output_images)
@@ -15,21 +15,25 @@ output_images = sys.argv[2]
 # it is used as the initial argument in python. It would be as if, 
 # sys,argv[0] is redundant, and that the computer knows it's python.
 
-if not os.path.exists(output_images):
-    os.makedirs(output_images)
+def make_dir(output_images):
+    if not os.path.exists(output_images):
+        os.makedirs(output_images)
 # the output_images would be considered the sys.argv[3], without being
 # explicit about the statement, as os is doing the work to produce that folder.
 # The folder produced would be a folder that can be named anything,
 # such as png_images.
 
-for filename in os.listdir(image_folder):
-    img = Image.open(f'./{image_folder}{filename}')
-    reduced_name = os.path.splitext(filename)[0]
-    img.save(f'{output_images}{reduced_name}.png', 'png')
-    print('success!!!')
-    img.show()
+def main(args):
+    make_dir(output_images)
+    for filename in os.listdir(image_folder):
+        img = Image.open(f'./{image_folder}{filename}')
+        reduced_name = os.path.splitext(filename)[0]
+        img.save(f'{output_images}{reduced_name}.png', 'png')
+        img.thumbnail((150,150))
+        print(img.size)
+        # print('success!!!')
+        img.show()
 
 
-# print(img.format)
-# print(img.size)
-# print(img.mode)
+if __name__ == '__main__':
+    main(sys.argv[1:])
